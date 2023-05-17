a12ea7ad6d16fab3a7828e70c576ab64b902eeba
Nick Nazarov <justmove8@gmail.com>
2023-05-17 22:31:06 +0300

    02 add logic to client

--- client/frontend.js
+++ client/frontend.js
@@ -1,1 +1,36 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+
+const App = {
+    data() {
+        return {
+            form: {
+                name: '',
+                value: ''
+            },
+            contacts: [
+                { id:1, name: 'Nick', value: '+7-921-100-20-30', marked: false }
+            ]
+        }
+    },
+    computed: {
+      canCreate() {
+          return this.form.value.trim() && this.form.name.trim()
+      }
+    },
+    methods: {
+         createContact() {
+             const {...contact} = this.form
+             this.contacts.push({...contact, id: Date.now(), marked: false})
+             this.form.name = this.form.value = ''
+         },
+        markContact(id) {
+            const contact = this.contacts.find(c => c.id === id)
+            contact.marked = true
+        },
+        removeContact(id) {
+            this.contacts = this.contacts.filter(c => c.id !== id)
+        }
+    }
+}
+
+createApp(App).mount('#app')
